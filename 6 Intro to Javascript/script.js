03c9f345aef67fbc59366b25866444b69ca6d73c
Quibfiddle <quibfiddle@gmail.com>
2017-09-12 14:32:59 -0700

    All the files as of 9-12
    Uploading all the notes, projects and files generates so far in my class. section 8 is not yet included, and iwll need to be updated later

---
+++ 6 Intro to Javascript/script.js
@@ -0,0 +1,5 @@
+var userName = prompt("what is your name?");
+
+alert("Nice to meet you," + userName);
+
+console.log("Also greate to meet you in here")
---
+++ 7 Javascript control flow/annoy.js
@@ -0,0 +1,8 @@
+var ans = prompt("are we there yet?");
+
+while(ans.indexOf("yes") == -1){
+	var ans = prompt("are we there yet?");
+}
+
+
+alert("Yay, We Made it!!!!");
