1dfcf61e3d6b45f64586f0875068d571459451f4
Quibfiddle <quibfiddle@gmail.com>
2017-09-21 17:53:33 -0700

    Fixed Dom manipulation of Logans Project
    Fixed Dom manipulation of Logans Project

--- 14 JQuery/Maths/logansproject2.js
+++ 14 JQuery/Maths/logansproject2.js
@@ -1,11 +1,27 @@
-var inputOne = document.querySelector("#numberOne");
-var inputTwo = document.querySelector("#numberTwo");
-var plus = document.querySelector("#plus")
-var subtract = document.querySelector("#subtract")
-var multiply = document.querySelector("#multiply")
-var divide = document.querySelector("#divide")
-var result = document.querySelector("H1");
+var inputOne = document.querySelector("#number1");
+var inputTwo = document.querySelector("#number2");
+var operation = document.querySelector("#operation");
+var submit = document.querySelector("#submit");
+var result = document.querySelector("#result");
 
-plus.addEventListener("click", function(){
-	alert(HEY);
-});
+submit.addEventListener("click", function(){
+	console.log(operation.value);
+	if(operation.value === "add"){
+		output = Number(inputOne.value) + Number(inputTwo.value);
+		result.textContent = output;
+	}
+	else if(operation.value === "subtract"){
+		output = Number(inputOne.value) - Number(inputTwo.value);
+		result.textContent = output;
+	}
+	else if(operation.value === "multiply"){
+		output = Number(inputOne.value) * Number(inputTwo.value);
+		result.textContent = output;		
+	}
+	else if(operation.value === "divide"){
+		output = Number(inputOne.value) / Number(inputTwo.value);
+		result.textContent = output;		
+	};
+
+	});
+
