27bb35890759f009885652b1ba6469bca4cf2626
Quibfiddle <quibfiddle@gmail.com>
2017-09-26 11:00:11 -0700

    Began Intro to BAckend
    Began intro to backend, fixed keycode css problem

--- 15 Advanced JQuery/KeyCodeProject/keyCode.js
+++ 15 Advanced JQuery/KeyCodeProject/keyCode.js
@@ -4,6 +4,9 @@ $("body").keypress(function(event){
 	if(String.fromCharCode(event.which) === " "){
 		$(".jumbotron").text("SpaceBar")
 	}
+	else if(event.which === 13){
+		$(".jumbotron").text("Enter");
+	}
 	else{
 	$(".jumbotron").text(String.fromCharCode(event.which));
 	}
